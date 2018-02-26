0acad03dee360cd2eadf39a59278f3b7e1f79b36
vaijuGore <sanjivaniborgaonkar@gmail.com>
2018-02-26 09:15:42 +0000

    This is to practice more repositories in GIT

---
+++ Documents/NetBeansProjects/TwoNumbersApplication/src/twonumbersapplication/TwoNumbers.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twonumbersapplication;
+
+/**
+ *
+ * @author sudhi
+ */
+public class TwoNumbers {
+    private int a;
+    private int b;
+
+    public TwoNumbers(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+   public int first(){
+    return a;
+
+} 
+    public int second(){
+    return b;
+} 
+     public int max(){
+         if (a>b){return a;}
+    return b;
+}  public boolean  equal(){
+    if (a==b)
+    {return true;}
+    return false;
+} 
+
+   
+    
+ 
+    
+
+
---
+++ Documents/NetBeansProjects/TwoNumbersApplication/src/twonumbersapplication/TwoNumbersApplication.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twonumbersapplication;
+
+/**
+ *
+ * @author sudhi
+ */
+public class TwoNumbersApplication {
+
+  
+    public static void main(String[] args) {
+        int a =234;
+       int b =345;
+       TwoNumbers myNumber = new TwoNumbers(a,b);
+       TwoNumbers X =  new TwoNumbers(12,18);
+        TwoNumbers Y =  new TwoNumbers(17,17);
+      int result = myNumber.first();
+        System.out.println(""+result);
+       myNumber.max();
+       myNumber.second();
+       myNumber.equal();
+       Boolean re2 = myNumber.equal();
+        System.out.println(""+re2);
+      String re1= myNumber.toString();
+       System.out.println(""+re1);
+    }
+    
+}
